d1bc79810ba2c15c979f0d4a3daa75f45815ad0c
Vortex1806 <shubhvora03@gmail.com>
2024-01-14 09:51:22 +0530

    yoga-module added

---
+++ Yoga-Module/data.py
@@ -0,0 +1,8 @@
+AngleData = [
+    {'Name': 'tadasan', 'right_arm': 160, 'left_arm': 200, 'right_leg':330,'left_leg':180},
+    {'Name': 'vrksana', 'right_arm': 160, 'left_arm': 200, 'right_leg':330,'left_leg':180},
+    {'Name': 'balasana', 'right_arm': 155, 'left_arm': 167, 'right_leg':337,'left_leg':335},
+    {'Name': 'trikonasana', 'right_arm': 181, 'left_arm': 184, 'right_leg':176,'left_leg':182},
+    {'Name': 'virabhadrasana', 'right_arm': 167, 'left_arm': 166, 'right_leg':273,'left_leg':178},
+    {'Name': 'adhomukha', 'right_arm': 176, 'left_arm': 171, 'right_leg':177,'left_leg':179}
+    ]
